2e79276ebc0a3e133af59778f15b73413d25ed83
yashK <yashkinariwalask95@gmail.com>
2018-08-12 17:56:15 -0400

    done

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'rual pandey',
+    profilePic: 'https://images.pexels.com/photos/894723/pexels-photo-894723.jpeg?cs=srgb&dl=fashion-man-person-894723.jpg&fm=jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
